c42dbe718a3d981937b3cf3110bd50d28d3aeb35
Roy Montoya <roymontoya89@gmail.com>
2018-05-29 16:34:41 -0700

    fixed updating list

--- app/src/main/java/com/test/finero/finerio/activities/MovimientosActivity.kt
+++ app/src/main/java/com/test/finero/finerio/activities/MovimientosActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.View
 import com.test.finero.finerio.R
 import com.test.finero.finerio.adapters.MovimientosAdapter
@@ -28,12 +27,14 @@ class MovimientosActivity : AppCompatActivity() {
 
     var pagination = 10
     var movimientosOnRequest = false
+    var movimientos: MutableList<Movimiento> = mutableListOf()
+    var adapter = MovimientosAdapter(movimientos)
 
     val queryMap: MutableMap<String, String> = hashMapOf(
             "deep" to "true",
             "includeCharges" to "true",
             "includeDeposits" to "true",
-            "max" to "10",
+            "max" to pagination.toString(),
             "tmz" to "GMT-07:00",
             "startDate" to (Dates.today - 30.days).toString(StringUtility.DATE_FORMAT),
             "endDate" to Dates.today.toString(StringUtility.DATE_FORMAT)
@@ -43,11 +44,11 @@ class MovimientosActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movimientos)
         setUserName()
-        callMovimientos()
+        callMovimientos(false)
+    }
+
+    private fun callMovimientos(isUpdating: Boolean) {
         movimientos_progress.visibility = View.VISIBLE
-    }
-
-    private fun callMovimientos() {
         movimientosOnRequest = true
         if (intent.extras != null) {
             FinerioNetwork.service.movimientosCall(intent.extras.get(StringUtility.ID_EXTRA).toString(),
@@ -56,14 +57,21 @@ class MovimientosActivity : AppCompatActivity() {
                 override fun onFailure(call: Call<MovimientosResponse>?, t: Throwable?) {
                     movimientosOnRequest = false
                     movimientos_progress.visibility = View.GONE
-                    Log.d(MovimientosActivity::class.java.simpleName, "fail")
                 }
 
                 override fun onResponse(call: Call<MovimientosResponse>?, response: Response<MovimientosResponse>?) {
                     movimientosOnRequest = false
                     movimientos_progress.visibility = View.GONE
                     if (response?.raw()?.code() == 200) {
-                        setMovimientoList(response.body()?.movimientos ?: arrayListOf())
+                        if (isUpdating) {
+                            movimientos.clear()
+                            movimientos.addAll(response.body()?.movimientos ?: mutableListOf())
+                            list_movimientos.adapter.notifyDataSetChanged()
+                        } else {
+                            movimientos.addAll(response.body()?.movimientos ?: mutableListOf())
+                            setMovimientoList()
+                        }
+
                     }
                 }
             })
@@ -71,47 +79,22 @@ class MovimientosActivity : AppCompatActivity() {
 
     }
 
-    private fun callMovimientoUpdate(){
-        movimientosOnRequest = true
-        if (intent.extras != null) {
-            FinerioNetwork.service.movimientosCall(intent.extras.get(StringUtility.ID_EXTRA).toString(),
-                    queryMap,
-                    intent.extras.get(StringUtility.AUTH_EXTRA).toString()).enqueue(object : Callback<MovimientosResponse> {
-                override fun onFailure(call: Call<MovimientosResponse>?, t: Throwable?) {
-                    movimientosOnRequest = false
-                    movimientos_progress.visibility = View.GONE
-                    Log.d(MovimientosActivity::class.java.simpleName, "fail")
-                }
-
-                override fun onResponse(call: Call<MovimientosResponse>?, response: Response<MovimientosResponse>?) {
-                    movimientosOnRequest = false
-                    movimientos_progress.visibility = View.GONE
-                    if (response?.raw()?.code() == 200) {
-//                        setMovimientoList(response.body()?.movimientos ?: arrayListOf())
-                    }
-                }
-            })
-        }
-    }
-
-    private fun setMovimientoList(movimientos: List<Movimiento>) {
+    private fun setMovimientoList() {
         val linearLayoutManager = LinearLayoutManager(this)
-        var adapter = MovimientosAdapter(movimientos)
         list_movimientos.setHasFixedSize(true)
         list_movimientos.layoutManager = linearLayoutManager
         list_movimientos.adapter = adapter
+        list_movimientos.adapter.notifyDataSetChanged()
         list_movimientos.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
                 val totalItemCount = recyclerView?.layoutManager?.itemCount
                 if (totalItemCount == linearLayoutManager.findLastVisibleItemPosition() + 1 && !movimientosOnRequest) {
-                     if(pagination + 10 >= totalItemCount){
-                         pagination +=10
-                         queryMap["max"] = (pagination).toString()
-                         callMovimientoUpdate()
-                     }else{
-
-                     }
+                    if (pagination + 10 >= totalItemCount) {
+                        pagination += 10
+                        queryMap["max"] = (pagination).toString()
+                        callMovimientos(true)
+                    }
                 }
             }
         })
@@ -128,4 +111,5 @@ class MovimientosActivity : AppCompatActivity() {
             noButton { }
         }.show()
     }
+
 }
