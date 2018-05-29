784999bdf8b9474de26c6af4c1ff458b4fec39f2
Roy Montoya <roymontoya89@gmail.com>
2018-05-29 16:14:57 -0700

    working with resets the view

--- app/src/main/java/com/test/finero/finerio/activities/MovimientosActivity.kt
+++ app/src/main/java/com/test/finero/finerio/activities/MovimientosActivity.kt
@@ -3,6 +3,7 @@ package com.test.finero.finerio.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
 import com.test.finero.finerio.R
@@ -11,7 +12,10 @@ import com.test.finero.finerio.responseObjects.Movimiento
 import com.test.finero.finerio.responseObjects.MovimientosResponse
 import com.test.finero.finerio.utility.FinerioNetwork
 import com.test.finero.finerio.utility.StringUtility
-import khronos.*
+import khronos.Dates
+import khronos.days
+import khronos.minus
+import khronos.toString
 import kotlinx.android.synthetic.main.activity_movimientos.*
 import org.jetbrains.anko.alert
 import org.jetbrains.anko.noButton
@@ -22,10 +26,14 @@ import retrofit2.Response
 
 class MovimientosActivity : AppCompatActivity() {
 
-    val queryMap: Map<String, String> = hashMapOf(
+    var pagination = 10
+    var movimientosOnRequest = false
+
+    val queryMap: MutableMap<String, String> = hashMapOf(
             "deep" to "true",
             "includeCharges" to "true",
             "includeDeposits" to "true",
+            "max" to "10",
             "tmz" to "GMT-07:00",
             "startDate" to (Dates.today - 30.days).toString(StringUtility.DATE_FORMAT),
             "endDate" to Dates.today.toString(StringUtility.DATE_FORMAT)
@@ -36,21 +44,24 @@ class MovimientosActivity : AppCompatActivity() {
         setContentView(R.layout.activity_movimientos)
         setUserName()
         callMovimientos()
-        progressbar_movimientos.visibility = View.VISIBLE
+        movimientos_progress.visibility = View.VISIBLE
     }
 
     private fun callMovimientos() {
+        movimientosOnRequest = true
         if (intent.extras != null) {
             FinerioNetwork.service.movimientosCall(intent.extras.get(StringUtility.ID_EXTRA).toString(),
                     queryMap,
                     intent.extras.get(StringUtility.AUTH_EXTRA).toString()).enqueue(object : Callback<MovimientosResponse> {
                 override fun onFailure(call: Call<MovimientosResponse>?, t: Throwable?) {
-                    progressbar_movimientos.visibility = View.GONE
+                    movimientosOnRequest = false
+                    movimientos_progress.visibility = View.GONE
                     Log.d(MovimientosActivity::class.java.simpleName, "fail")
                 }
 
                 override fun onResponse(call: Call<MovimientosResponse>?, response: Response<MovimientosResponse>?) {
-                    progressbar_movimientos.visibility = View.GONE
+                    movimientosOnRequest = false
+                    movimientos_progress.visibility = View.GONE
                     if (response?.raw()?.code() == 200) {
                         setMovimientoList(response.body()?.movimientos ?: arrayListOf())
                     }
@@ -60,10 +71,50 @@ class MovimientosActivity : AppCompatActivity() {
 
     }
 
+    private fun callMovimientoUpdate(){
+        movimientosOnRequest = true
+        if (intent.extras != null) {
+            FinerioNetwork.service.movimientosCall(intent.extras.get(StringUtility.ID_EXTRA).toString(),
+                    queryMap,
+                    intent.extras.get(StringUtility.AUTH_EXTRA).toString()).enqueue(object : Callback<MovimientosResponse> {
+                override fun onFailure(call: Call<MovimientosResponse>?, t: Throwable?) {
+                    movimientosOnRequest = false
+                    movimientos_progress.visibility = View.GONE
+                    Log.d(MovimientosActivity::class.java.simpleName, "fail")
+                }
+
+                override fun onResponse(call: Call<MovimientosResponse>?, response: Response<MovimientosResponse>?) {
+                    movimientosOnRequest = false
+                    movimientos_progress.visibility = View.GONE
+                    if (response?.raw()?.code() == 200) {
+//                        setMovimientoList(response.body()?.movimientos ?: arrayListOf())
+                    }
+                }
+            })
+        }
+    }
+
     private fun setMovimientoList(movimientos: List<Movimiento>) {
+        val linearLayoutManager = LinearLayoutManager(this)
+        var adapter = MovimientosAdapter(movimientos)
         list_movimientos.setHasFixedSize(true)
-        list_movimientos.layoutManager = LinearLayoutManager(this)
-        list_movimientos.adapter = MovimientosAdapter(movimientos)
+        list_movimientos.layoutManager = linearLayoutManager
+        list_movimientos.adapter = adapter
+        list_movimientos.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                val totalItemCount = recyclerView?.layoutManager?.itemCount
+                if (totalItemCount == linearLayoutManager.findLastVisibleItemPosition() + 1 && !movimientosOnRequest) {
+                     if(pagination + 10 >= totalItemCount){
+                         pagination +=10
+                         queryMap["max"] = (pagination).toString()
+                         callMovimientoUpdate()
+                     }else{
+
+                     }
+                }
+            }
+        })
     }
 
     private fun setUserName() {
